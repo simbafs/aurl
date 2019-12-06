f60957da213c3ec43ce1bf77f8f7cd50ffc81ebd
simba-fs <simba.fs@gmail.com>
2019-12-06 08:11:39 +0000

    improve head and foot

---
+++ public/lib/tmp.sh
@@ -0,0 +1,5 @@
+ln -s ../../node_modules/jquery/dist/jquery.min.js jquery.js
+ln -s ../../node_modules/clipboard/dist/clipboard.min.js clipboard.js
+ln -s ../../node_modules/alertifyjs/build/alertify.min.js alertify.js
+ln -s ../../node_modules/alertifyjs/build/css/alertify.min.css alertify.css
+ln -s ../../node_modules/alertifyjs/build/css/themes/default.min.css alertify.theme.css
