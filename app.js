961d6bd66d58ef1232efb1920d895f7433edf871
simba <simba.fs@gmail.com>
2019-12-06 23:55:26 +0800

    add favicon.ico

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// favicon setup
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
