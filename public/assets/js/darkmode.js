e406aa68c5d8d00596b8b281eeff31bb9e0d303d
simba <simba.fs@gmail.com>
2020-01-09 22:53:03 +0800

    test

--- public/assets/js/darkmode.js
+++ public/assets/js/darkmode.js
@@ -1,12 +1,1 @@
-var options = {
-  mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#fff',  // default: '#fff'
-  buttonColorDark: '#100f2c',  // default: '#100f2c'
-  buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: true, // default: true,
-  label: '🌓', // default: ''
-  autoMatchOsTheme: true // default: true
-}
-
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
+new Darkmode().showWidget();
