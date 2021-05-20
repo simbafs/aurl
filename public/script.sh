c6992acb228aa04ad61dced0f888e84f093212e8
simba-fs <simba.fs@gmail.com>
2021-05-20 13:47:23 +0800

    clean

--- public/script.sh
+++
@@ -1,9 +0,0 @@
-# rm assets -rf
-mv -f bss/* . 
-rename 's/html/ejs/' *
-sed -i 's/&lt;/</g' *.ejs
-sed -i 's/&gt;/>/g' *.ejs
-sed -i 's/&nbsp;/ /g' *.ejs
-sed -i 's/assets/\/assets/g' *.ejs
-rm -rf ../views/*
-mv -f *.ejs ../views/
