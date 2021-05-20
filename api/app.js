2570b5036f304897b2ee9cd67422ea2fe666891c
simba-fs <simba.fs@gmail.com>
2021-05-20 15:56:02 +0800

    (api) install packages

---
+++ api/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/index'));
+
+module.exports = app;
