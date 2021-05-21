388c670e94f5e9df7af7ad5860d94fba5030dba1
simba-fs <simba.fs@gmail.com>
2021-05-21 09:19:21 +0800

    (api) remove bin/www

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,22 @@
+import config from 'config';
+import Debug from 'debug';
+const debug = Debug('api:app');
+
 import express from'express';
 import path from'path';
 import cookieParser from'cookie-parser';
 import logger from'morgan';
 import helmet from'helmet';
 import cors from'cors';
+
+import mongoose from 'mongoose';
+mongoose.connect(config.get('DB'), {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}, (err) => {
+	if(err) return console.error(err);
+	debug('DB connect');
+});
 
 const app = express();
 
@@ -16,5 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
+
+app.listen(config.get('port'));
 
 module.exports = app;
